e0e64feffa76e65e76bd2480143993c73fce7976
milosbugarinovic <milos.bugarinovic@gmail.com>
2025-03-20 09:26:35 +0100

    fix: update error handling in express middleware and refine constructor parameters, lint issue

--- src/express/middleware.ts
+++ src/express/middleware.ts
@@ -1,13 +1,27 @@
 import { type NextFunction, type Request, type Response } from 'express'
 
-import { type ErrorBaseModel } from '#src/error/base-model'
+import { ErrorBaseModel } from '#src/error/base-model'
 
-export const expressMiddleware = (err: Error | ErrorBaseModel, _req: Request, res: Response, _next: NextFunction): unknown => {
-	const code = (err as ErrorBaseModel).httpCode ?? 500
+export const expressMiddleware = (err: unknown, _req: Request, res: Response, _next: NextFunction): unknown => {
+	if (err instanceof ErrorBaseModel) {
+		return res.status(err.httpCode).json({
+			code: err.httpCode,
+			message: err.message,
+			status: 'error',
+		})
+	}
 
-	return res.status(code).json({
-		code,
-		message: err.message,
+	if (err instanceof Error) {
+		return res.status(500).json({
+			code: 500,
+			message: err.message,
+			status: 'error',
+		})
+	}
+
+	return res.status(500).json({
+		code: 500,
+		message: 'Unknown error',
 		status: 'error',
 	})
 }
